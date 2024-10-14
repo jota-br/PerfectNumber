cfb9ffbe95879c6db60602a00fd234aca28e0f11
João Carlos Roveda Ostrovski <ojcroveda@gmail.com>
2024-10-14 09:00:27 -0300

    Update PerfectNumber.java

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -1,17 +1,16 @@
 public class PerfectNumber {
-    public static boolean isPerfectNumber(int number) {
-        if(number < 1) {
+    public static boolean isPerfectNumber (int number) {
+        if (number < 1) {
             return false;
         }
-        int i = 1, sum = 0, x;
-        while(i < number){
-            x = number % i;
-            if(x == 0) {
+        
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
                 sum += i;
             }
-            i++;
         }
-        System.out.println(sum);
-        return sum == number;
+        
+        return (number == sum);
     }
 }
